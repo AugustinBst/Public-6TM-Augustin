0406b49c02e8b231632466d198c8ccae26fd67c3
Augustin Bost <augustin.bost@epitech.eu>
2024-01-16 17:34:52 +0100

    FEAT (trombimania HomePage.vue): HEADER nav bar

--- Front/trombimania/src/App.vue
+++ Front/trombimania/src/App.vue
@@ -1,27 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HomePage/>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './views/HomePage.vue';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+
+export default {
+  components : {
+    HomePage
+  }
+}
 </script>
 
 <style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
