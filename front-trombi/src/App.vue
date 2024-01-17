2a61dd9c8303f94d21a357dd52811923493845cb
Augustin Bost <augustin.bost@epitech.eu>
2024-01-17 10:07:26 +0100

    FEAT Front test

---
+++ front-trombi/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HomePage/>
+</template>
+
+<script lang="ts">
+import HomePage from './views/HomePage.vue';
+
+
+export default {
+  components : {
+    HomePage
+  }
+}
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+#app {
+
+}
+</style>
